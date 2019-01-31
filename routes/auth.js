db78f69f50651db821108a8ce275cc13612869e6
shakil <36266736+sXakil@users.noreply.github.com>
2019-02-01 01:22:08 +0600

    temporary comment removed

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,16 @@ var express  = require("express");
 var router   = express.Router();
 var passport = require("passport");
 let db       = require("../models");
-//let flash    = require("connect-flash");
 let middleware = require("../middleware");
 
-//root route
 router.get("/", function(req, res){
     res.redirect("/blogs")
 });
 
-// show register form
 router.get("/register", middleware.isAlreadyLoggedIn, function(req, res){
     res.render("register", {error: req.flash('error')});
 });
 
-//handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new db.User({
         username: req.body.username,
@@ -42,19 +38,17 @@ router.post("/register", function(req, res){
         }
     });
 });
-//show login form
+
 router.get("/login", middleware.isAlreadyLoggedIn, function(req, res){
     res.render("login", {error: req.flash('error')});
 });
 
-//handling login logic
 router.post("/login", passport.authenticate("local", {
         successReturnToOrRedirect: "/blogs",
         failureRedirect: "/login",
         failureFlash: true
 }));
 
-// logout route
 router.get("/logout", function(req, res){
     req.logout();
     res.redirect("/blogs");
